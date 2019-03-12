af03b6591ca967d5d0490309d966d10a135b60ed
wanghao8080 <117663444@qq.com>
2019-03-13 00:30:32 +0800

    请求数据（getInitialProps）

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,31 @@
 import Link from 'next/link'
+import http from 'axios'
 import MainLayout from '../components/MainLayout'
 
 const ListItem = (props) => (
   <li>
-    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+    <Link as={`/p/${props.id}`} href={`/post?id=${props.id}`}>
       <a>{props.title}</a>
     </Link>
   </li>
 )
 
-const Index = () => (
+const Index = (props) => (
   <MainLayout>
     <ul>
-      <ListItem id='hello-ninghao' title='hello ninghao.net' />
-      <ListItem id='learn-next' title='learn next.js from ninghao' />
+      {props.posts.map(item => (
+        <ListItem key={item.id} id={item.id} title={item.title} />
+      ))}
     </ul>
   </MainLayout>
 )
+
+Index.getInitialProps = async () => {
+  const response = await http.get('http://localhost:3333/posts')
+
+  return {
+    posts: response.data
+  }
+}
 
 export default Index
--- pages/post.js
+++ pages/post.js
@@ -1,10 +1,21 @@
-import MainLayout from '../components/MainLayout'
+import http from 'axios'
 import { withRouter } from 'next/router'
+import MainLayout from '../components/MainLayout'
 
 const Post = withRouter((props) => (
   <MainLayout>
-    <p>{props.router.query.title}</p>
+    <h3>{props.post.title}</h3>
+    <p>{props.post.description}</p>
   </MainLayout>
 ))
+
+Post.getInitialProps = async (context) => {
+  const { id } = context.query
+  const response = await http.get(`http://localhost:3333/posts/${id}`)
+
+  return {
+    post: response.data
+  }
+}
 
 export default Post
